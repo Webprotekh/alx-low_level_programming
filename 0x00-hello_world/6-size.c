e764968796f74dabb6d7cc1b5dd5c175b7d4348d
Webprotekh <webprotekh@gmail.com>
2021-08-28 08:49:31 -0700

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,10 @@
 
 int main(void)
 {
-	printf(sizeof);
+	printf("Size of a char: %d", sizeof(char));
+	printf("Size of an int: %d", sizeof(int));
+	printf("Size of a long int: %d", sizeof(long));
+	printf("Size of a long long int: %d", sizeof(long long));
+	printf("Size of a float: %d", sizeof(float));
 	return (0);
 }
