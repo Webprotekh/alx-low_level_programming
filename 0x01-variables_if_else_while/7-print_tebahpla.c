24c297f17da273b142b1d329b357cc8c58605fbf
Webprotekh <webprotekh@gmail.com>
2021-08-29 10:27:33 -0700

    inverse letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print inverse alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'z'; letters <= 'a'; letters++)
+		putchar(letters);
+	putchar('\n');
+	return (0);
+}
