f4510fff76709131f9b1e4f079a7b557cc9d5a3f
Webprotekh <webprotekh@gmail.com>
2021-08-30 00:20:40 -0700

    comb 4 file aDDED

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - comb 4
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j, k, l;
+
+	while (i <= 999)
+	{
+		j = (i / 100 + '0');
+		k = (i / 10 % 10 + '0');
+		l = (i % 10 + '0');
+
+		if ((j < k) && (k < l))
+		{
+			putchar(j);
+			putchar(k);
+			putchar(l);
+
+			if (i != 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		i++
+	}
+	putchar('\n');
+	return (0);
+}
