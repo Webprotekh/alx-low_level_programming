304631c3a50a2e05afdea615fbbb4c1fdb1d73e5
Webprotekh <webprotekh@gmail.com>
2021-08-29 23:42:44 -0700

    comb3 added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -
+ *
+ * Return: Alway 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	while (i <= 99)
+	{
+		j = (c / 10 + '0');
+		k = (c % 10 + '0');
+		if (j < k)
+		{
+			putchar(j);
+			putchar(k);
+			if (i != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
