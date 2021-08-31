c45d1232a30372a6f8179cd6ce3196c4a6c0cff1
Webprotekh <webprotekh@gmail.com>
2021-08-31 06:45:21 -0700

    updated

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-	char c[20] = "Holberton";
-	_putchar(c);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	return (0);
 }
