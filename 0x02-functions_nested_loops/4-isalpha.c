63e92b679242da8cfa23769c60885e8625fdbd27
Webprotekh <webprotekh@gmail.com>
2021-08-31 10:44:56 -0700

    isalpha file added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if it is lower or upper case
+ *
+ * c - the parameter to check
+ *
+ * Return: Either 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	int alpha = c;
+
+	if (alpha)
+		return (1);
+	else if (!alpha)
+		return (0);
+}
