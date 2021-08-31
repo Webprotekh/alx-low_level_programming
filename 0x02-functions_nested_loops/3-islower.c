69097926b72a29241d0ff57459edd87f8cd49d9f
Webprotekh <webprotekh@gmail.com>
2021-08-31 08:55:31 -0700

    islower file added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * islower - Checking for lower case
+ *
+ * Return: Always 1 or 0
+ */
+
+int islower(int c)
+{
+	char c;
+
+	if (c > 0)
+		return (1);
+	else
+		return (0);
+}
