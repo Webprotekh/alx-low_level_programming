188d6074c5117ceb1f17044958eb5edce9607c46
Webprotekh <webprotekh@gmail.com>
2021-08-31 10:14:17 -0700

    upod

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,17 @@
 /**
  * islower - Checking for lower case
  *
- * Return: Always 1 or 0
+ * c - returns 1 or 0
+ *
+ * Return: Always 0
  */
 
 int _islower(int c)
 {
-	int letter = c;
+	int letter = 'c';
 
 	if (letter)
-	{
 		return (1);
-	}
 	else
 		return (0);
 }
