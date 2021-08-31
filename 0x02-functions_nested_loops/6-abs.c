677ced32eda28b1ee92d81a37c0e2cfbd55c7452
Webprotekh <webprotekh@gmail.com>
2021-08-31 13:09:52 -0700

    abs file added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - printing the absolute numbers of an integer
+ *
+ * @: - parameter
+ *
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+	int x = 2;
+	return (0);
+}
