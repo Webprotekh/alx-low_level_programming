d66715820dd2cb7fbf3f96247ca47002a4112beb
Webprotekh <webprotekh@gmail.com>
2021-08-31 07:23:37 -0700

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,17 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
 	return (0);
 }
