9abb72d6f90613b50e9f58c0ff66ea82df480b06
Webprotekh <webprotekh@gmail.com>
2021-08-31 13:12:12 -0700

    updated

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,12 @@
 /**
  * _abs - printing the absolute numbers of an integer
  *
- * @: - parameter
+ * @i: - parameter
  *
  * Return: Always 0
  */
 
-int _abs(int)
+int _abs(int i)
 {
 	int x = 2;
 	return (0);
