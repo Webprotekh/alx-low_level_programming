6d4c72238b1af69c7122b0a7debfe811d4fc0a3b
Webprotekh <webprotekh@gmail.com>
2021-08-31 07:42:52 -0700

    x10 alphabet file added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alpabet_x10 - Printing the lowercase alphabet in 10 places
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char c;
+		
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
