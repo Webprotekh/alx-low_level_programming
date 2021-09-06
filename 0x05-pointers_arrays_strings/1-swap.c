5aef9cec6af9d13b1283cf9c39ee3436cef4fb3f
Webprotekh <webprotekh@gmail.com>
2021-09-06 09:36:59 -0700

    Swap file created

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,15 @@
 #include <main.h>
 
-/*
+/**
+ * swap_int - To swap the value of two integers
+ * @a: parameter to swap
+ * @b: Second parameter to swap
  *
- *
+ * Return: Always 0
  */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
