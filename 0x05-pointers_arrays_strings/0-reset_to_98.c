dce27190aa671c4af4eed586351e47f21d6503e8
Webprotekh <webprotekh@gmail.com>
2021-09-06 09:26:24 -0700

    updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/* reset_to_98 - To reset the value of the function to 98
- *
- * @*n: The parameter to change 
+/*
+ * reset_to_98 - To reset the value of the function to 98
+ * @n: The parameter to change 
  * 
  * Return: Always 0
  */
