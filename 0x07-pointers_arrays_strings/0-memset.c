5ac8f9d813546a78ec1a3229bfb34eab11ba8d37
Webprotekh <webprotekh@gmail.com>
2021-09-13 07:31:15 -0700

    updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * _memset - filling memory with constant bytes
+ * @s: parameter to print
+ * @b: parameter for the constant
+ * @n: parameter bytes for the memset
+ *
+ * Return: Always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
