81730fd8e56e2cfe70aa8beb0233cf49f4b61c56
Webprotekh <webprotekh@gmail.com>
2021-09-13 12:40:48 -0700

    updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,22 +10,24 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int j = 0, k;
-	int same = 0;
+int i = 0, j;
+int matches = 0;
 
-	while (s[j] != '\0')
-	{
-		for (k = 0; acxept[k] != '\0'; k++)
-		{
-			if (s[j] == accept[k])
-			{
-				same++;
-				break;
-			}
-			if (accept[k + 1] == '\0' && s[j] != accept[k])
-				return (same);
-		}
-		j++;
-	}
-	return (same);
+while (s[i] != '\0') /*iterate through string*/
+{
+
+for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+{
+if (s[i] == accept[j]) /*record & break at first match*/
+{
+matches++;
+break;
+}
+if (accept[j + 1] == '\0' && s[i] != accept[j])
+return (matches);/*return if idx doesn't match*/
+}
+i++;
+}
+return (matches); /* return num if all match till end */
+
 }
