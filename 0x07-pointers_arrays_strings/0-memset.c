ab12babf5f6210d75dba6016be197becb3baed72
Webprotekh <webprotekh@gmail.com>
2021-09-13 07:54:01 -0700

    commited

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,5 +12,23 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int j;
 
+	j = 0;
+	while (j < n)
+	{
+		if (j % 10)
+		{
+			_putchar(" ");
+		}
+		if (!(j % 10) && j)
+		{
+			_putchar('\n');
+		}
+		_putchar('0x%01x', buffer[j]);
+		j++;
+	}
+	_putchar('\n');
+
+	return 0;
 }
