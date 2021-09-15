ec1c2698c245f2f1017c6977fd49d569b16eca66
Webprotekh <webprotekh@gmail.com>
2021-09-15 06:45:23 -0700

    updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - To print the length of a string
- * @*s: The parameter to check
+ * @s: The parameter to check
  *
  * Return: The length of the string should be returned
  */
@@ -11,5 +11,5 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (_strlen_recursion(s + 1));
+	return (_strlen_recursion(*s + 1));
 }
