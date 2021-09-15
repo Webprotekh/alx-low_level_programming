ba4bad277eaeb42efd752c853fa4af8808c71de0
Webprotekh <webprotekh@gmail.com>
2021-09-15 04:02:37 -0700

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,10 +11,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	_putchar('\n');
 }
