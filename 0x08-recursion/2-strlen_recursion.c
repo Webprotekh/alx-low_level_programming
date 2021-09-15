c077b21815831e871b08dafe9f05fae65d8c096e
Webprotekh <webprotekh@gmail.com>
2021-09-15 07:00:27 -0700

    updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,7 +9,10 @@
 
 int _strlen_recursion(char *s)
 {
+	int i;
+	
+	i = 0;
 	if (*s == '\0')
-		return (0);
-	return (_strlen_recursion(*s + 1));
+		return (i);
+	return (_strlen_recursion(s + i));
 }
