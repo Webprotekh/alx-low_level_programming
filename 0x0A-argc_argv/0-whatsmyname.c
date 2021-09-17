e917b7a5e92ff71f2f1920a76b6331964e26e103
Webprotekh <webprotekh@gmail.com>
2021-09-17 02:04:07 -0700

    Files created

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <main.h>
+#include "main.h"
+
+/**
+ * main - To write a program that print out its name
+ * @argc: Parameter for the program count
+ * @argv[]: The parameter for the array
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	count = 0;
+	printf("%s\n", argv[count]);
+	return (0);
+}
