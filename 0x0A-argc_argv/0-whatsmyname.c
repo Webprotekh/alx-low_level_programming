023ec4c83e0fa25c56e89ca92e0d9735f196937f
Webprotekh <webprotekh@gmail.com>
2021-09-17 09:13:27 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,14 +4,14 @@
 /**
  * main - To write a program that print out its name
  * @argc: Parameter for the program count
- * @argv[]: The parameter for the array
+ * @argv: The parameter for the array
  *
  * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
+	if (argc >= 0)
 		printf("./%s\n", argv[0]);
 	return (0);
 }
