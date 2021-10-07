00c3a4abf23f4e2ce18aa3b9959107be789844ed
Webprotekh <webprotekh@gmail.com>
2021-10-07 13:34:59 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Function to pritn the multiplication of two numbers
+ * @a: The first Parameter to check
+ * @b: The second parameter to checkh
+ *
+ * Return: The result of mul
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
