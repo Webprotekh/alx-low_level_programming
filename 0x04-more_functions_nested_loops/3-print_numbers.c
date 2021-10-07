88f609b04acf26397d8916b0f477bfe9c9bcc70e
Webprotekh <webprotekh@gmail.com>
2021-10-07 13:41:11 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function to print numbers
+ * @void: Parameters to checkh
+ *
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+	int ctr;
+
+	for (ctr = 48; ctr < 58; ctr++)
+		_putchar(ctr);
+	_putchar('\n');
+}
