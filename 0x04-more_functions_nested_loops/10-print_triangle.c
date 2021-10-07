a48c6b9fb79bb4d39ff5c6de67b822823638634c
Webprotekh <webprotekh@gmail.com>
2021-10-07 07:53:21 -0700

    updated

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,26 +9,26 @@
 
 void print_triangle(int size)
 {
-    if (size > 0)
-    {
-        int row, col, i;
-
-        for (i = 0; i < size; i++)
-        {
-            for (row = size; row > i; row--)
-            {
-                _putchar(' ');
-            }
-            for (col = 0; col <= row; col++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-        _putchar('\n');
-    }
-    else if (size <= 0)
-    {
-        _putchar('\n');
-    }
+	if (size > 0)
+	{
+		int row, col, i;
+		
+		for (i = 0; i < size; i++)
+		{
+			for (row = size; row > i; row--)
+			{
+				_putchar(' ');
+			}
+			for (col = 0; col <= row; col++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+	else if (size <= 0)
+	{
+		_putchar('\n');
+	}
 }
